5150c5e1399c90c7fc48ef6eb8aff99e3e8650c7
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-18 15:16:34 +0530

    Initial commit

---
+++ main.ts
@@ -0,0 +1,12 @@
+-Returns the invesment buy requiring less human power
+-It can run tests 24/7
+-We can reuse the test scripts
+-Automation helps you find bugs in the early stages
+of software development,
+reducing expenses and working hours to fix these problems as well.
+-It is more fun since no repetitive testing
+-Also automating test allows to run tests on thousands of 
+mobile devices.Testing all of them manually would take forever
+In the end you will have a better quality software 
+which will be released earlier, with less problems and
+you have used less resources.
